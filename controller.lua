9bb79a6b6c82f51e7be951beb81f6a6065f8d7dc
Aleksey <astynax84@gmail.com>
2015-10-21 18:05:32 +0300

    input handling in controller.lua

--- controller.lua
+++ controller.lua
@@ -1,10 +1,30 @@
-num = 0
+num = nil
 
-function loop(arg)
-  num = num + 1
-  writeLn(num .. ") " .. arg .. "!")
-  setInput("")
-  scrollToEnd()
+function newGame()
+   num = tostring(math.random(10000))
+   writeLn("New game started!")
 end
 
-writeLn("Hello, it's Lua!")
+function loop(arg)
+   if not(arg == "") then
+      if arg == "n" then
+         newGame()
+
+      elseif arg == "h" or arg == "?" then
+         -- TODO: show help!
+
+      elseif string.match(arg, "^%d%d%d%d$") then
+         input = tonumber(arg)
+         -- TODO: complete the gameplay cycle
+
+      else
+         writeLn([[
+Bad input! Enter 'n', 'h', '?', or 4-digit numbers, please.]])
+      end
+   end
+   setInput("")
+   scrollToEnd()
+end
+
+writeLn([[Hello, this is "Bull & Cows" Game!]])
+newGame()
