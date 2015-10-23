10c3269552d73ffe26a938f2aecea847b8c64f66
Aleksey <astynax84@gmail.com>
2015-10-23 15:28:18 +0300

    help message

--- controller.lua
+++ controller.lua
@@ -11,7 +11,17 @@ function loop(arg)
          newGame()
 
       elseif arg == "h" or arg == "?" then
-         -- TODO: show help!
+         writeLn([[
+The "Bulls & Cows" Game.
+To win a game you should guess a four-digit number.
+On each of your tries program tell how many cows and bulls this try has.
+"Bull" means what you have a one correct digit at the correct place.
+"Cow" means what you have a one correct digit that placed incorrectly.
+
+Available commands:
+- 'n' - start new game,
+- 'h' or '?' - show this help,
+]])
 
       elseif string.match(arg, "^%d%d%d%d$") then
          input = tonumber(arg)
@@ -19,7 +29,7 @@ function loop(arg)
 
       else
          writeLn([[
-Bad input! Enter 'n', 'h', '?', or 4-digit numbers, please.]])
+Bad input! Enter 'n', 'h', '?', or 4-digit number, please.]])
       end
    end
    setInput("")
